ebe7dee3d68c8abe4ec33234f96269c2aa0094ac
Ryan Adi Putra <ryannadiputraa@gmail.com>
2021-03-25 16:54:37 +0800

    create db connection config and change item model price from float32 to int to match db schema

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ryanadiputraa/gervichstore-server
 
 go 1.16
+
+require github.com/lib/pq v1.10.0 // indirect
--- models/itemsModel.go
+++ models/itemsModel.go
@@ -4,7 +4,7 @@ type Items struct {
 	ID string `json:"id"`
 	Image string `json:"image"`
 	Name string `json:"name"`
-	Price float32 `json:"price"`
+	Price int `json:"price"`
 	Stock int `json:"stock"`
 	Category string `json:"category"`
 }
