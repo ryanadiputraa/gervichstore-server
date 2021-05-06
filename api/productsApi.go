17c7e1c2502fa1d3ff01906c4df0ff37130f628b
Ryan Adi Putra <ryannadiputraa@gmail.com>
2021-05-06 09:35:59 +0800

    refactor update product api to store product image in server and delete the old one

--- api/productsApi.go
+++ api/productsApi.go
@@ -74,6 +74,46 @@ func (*ProductHandlers) GetAllProducts(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
+// GetProduct is an api handler to find certain product in db based on product id
+func(*ProductHandlers) GetProduct(w http.ResponseWriter, r *http.Request) {
+	db, err := config.OpenConnection()
+	if err != nil {
+		helpers.WriteErrorResponse(w, r, http.StatusBadGateway, "bad gateway")
+		return
+	}
+
+	productId := helpers.GetURLParams(r, 3)
+	row, err := db.Query(fmt.Sprintf("SELECT * FROM products WHERE id=%v", productId))
+	if err != nil {
+		helpers.WriteErrorResponse(w, r, http.StatusBadGateway, "bad gateway")	
+		return	
+	}
+
+	var product models.Product
+	isNotNull := row.Next()
+	if !isNotNull {
+		helpers.WriteErrorResponse(w, r, http.StatusNotFound, "not found")	
+		return	
+	}
+	row.Scan(&product.ID, &product.Image, &product.Name, &product.Price, &product.Stock, &product.Category, &product.CreatedAt, &product.UpdatedAt)
+
+	response := models.ProductResponseFormat {
+		Code: http.StatusOK,
+		Data: product,
+	}
+	
+	jsonBytes, err := json.Marshal(response)
+	if err != nil {
+		helpers.WriteInternalServerError(w, r)
+		return	
+	}
+
+	helpers.WriteResponse(w, r, "application/json", http.StatusOK, jsonBytes)
+	defer row.Close()
+	defer db.Close()
+	return
+}
+
 // CreateProduct is an api handler to post new product to db
 func(*ProductHandlers) CreateProduct(w http.ResponseWriter, r *http.Request) {
 	db, err := config.OpenConnection()
@@ -143,46 +183,6 @@ func(*ProductHandlers) CreateProduct(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-// GetProduct is an api handler to find certain product in db based on product id
-func(*ProductHandlers) GetProduct(w http.ResponseWriter, r *http.Request) {
-	db, err := config.OpenConnection()
-	if err != nil {
-		helpers.WriteErrorResponse(w, r, http.StatusBadGateway, "bad gateway")
-		return
-	}
-
-	productId := helpers.GetURLParams(r, 3)
-	row, err := db.Query(fmt.Sprintf("SELECT * FROM products WHERE id=%v", productId))
-	if err != nil {
-		helpers.WriteErrorResponse(w, r, http.StatusBadGateway, "bad gateway")	
-		return	
-	}
-
-	var product models.Product
-	isNotNull := row.Next()
-	if !isNotNull {
-		helpers.WriteErrorResponse(w, r, http.StatusNotFound, "not found")	
-		return	
-	}
-	row.Scan(&product.ID, &product.Image, &product.Name, &product.Price, &product.Stock, &product.Category, &product.CreatedAt, &product.UpdatedAt)
-
-	response := models.ProductResponseFormat {
-		Code: http.StatusOK,
-		Data: product,
-	}
-	
-	jsonBytes, err := json.Marshal(response)
-	if err != nil {
-		helpers.WriteInternalServerError(w, r)
-		return	
-	}
-
-	helpers.WriteResponse(w, r, "application/json", http.StatusOK, jsonBytes)
-	defer row.Close()
-	defer db.Close()
-	return
-}
-
 // UpdateProduct is an api handler to update certain product in db based on product id
 func(*ProductHandlers) UpdateProduct(w http.ResponseWriter, r *http.Request) {
 	db, err := config.OpenConnection()
@@ -191,11 +191,64 @@ func(*ProductHandlers) UpdateProduct(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// delete current image
 	productId := helpers.GetURLParams(r, 3)
+	row, err := db.Query(fmt.Sprintf("SELECT image FROM products WHERE id = %v", productId))
+	if err != nil {
+		helpers.WriteErrorResponse(w, r, http.StatusBadGateway, "bad gateway")	
+		return	
+	}
+	var currentProduct models.Product
+	isNotNull := row.Next()
+	if !isNotNull {
+		helpers.WriteErrorResponse(w, r, http.StatusNotFound, "not found")	
+		return	
+	}
+	row.Scan(&currentProduct.Image)
+	currentProduct.Image = strings.Split(currentProduct.Image, "/")[1]
+	if _, err := os.Stat(fmt.Sprintf("%v/uploads/%v",os.Getenv("WORKING_DIRECTORY") ,currentProduct.Image)); !os.IsNotExist(err) {
+		fmt.Print("File exist")
+		if err = os.Remove(fmt.Sprintf("%v/uploads/%v",os.Getenv("WORKING_DIRECTORY"), currentProduct.Image)); err != nil {
+			helpers.WriteInternalServerError(w, r)
+			return
+		}
+	}
+	
+	// assign product data
 	var updatedProduct models.Product
-	err = json.NewDecoder(r.Body).Decode(&updatedProduct)
+	updatedProduct.Name = r.FormValue("name")
+	updatedProduct.Price, err = strconv.Atoi(r.FormValue("price"))
+	if err != nil {
+		helpers.WriteErrorResponse(w, r, http.StatusBadRequest, "price must be int")
+		return
+	}
+	updatedProduct.Stock, err = strconv.Atoi(r.FormValue("stock"))
+	if err != nil {
+		helpers.WriteErrorResponse(w, r, http.StatusBadRequest, "stock must be int")
+		return
+	}
+	updatedProduct.Category = r.FormValue("category")
+	
+	// store and assign product image
+	r.ParseMultipartForm(10 << 20)
+	file, handler, err := r.FormFile("image")
 	if err != nil {
 		helpers.WriteErrorResponse(w, r, http.StatusBadRequest, "bad request")
+		return
+	}
+	defer file.Close()
+
+	updatedProduct.Image = fmt.Sprintf("%v/%v", os.Getenv("BASE_URL"), handler.Filename)
+	fileLocation := filepath.Join("uploads", handler.Filename)
+	targetFile, err := os.OpenFile(fileLocation, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		helpers.WriteInternalServerError(w, r)
+		return
+	}
+	defer targetFile.Close()
+
+	if _, err := io.Copy(targetFile, file); err != nil {
+		helpers.WriteInternalServerError(w, r)
 		return
 	}
 
